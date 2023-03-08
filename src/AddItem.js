db7583ae5cc06873856d45edeb3b8eb356efb210
Mekael Turner <mistnpeace@gmail.com>
2023-03-08 14:46:06 +0800

    Correct button focus issue

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,10 +1,14 @@
 import { FaPlus } from 'react-icons/fa';
+import { useRef } from 'react';
+
 export default function AddItem({ newItem, setNewItem, handleSubmit }) {
+  const inputRef = useRef();
   return (
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="addItem"
         type="text"
         placeholder="Add Item"
@@ -12,7 +16,11 @@ export default function AddItem({ newItem, setNewItem, handleSubmit }) {
         value={newItem}
         onChange={event => setNewItem(event.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
